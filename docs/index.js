c59ed27eca8dfabb336c22bd27b33587514db894
John Kayne <johnhoganuke2@gmail.com>
2018-01-18 20:58:30 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,27 @@ function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 }
 
-(function() {
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function getClientIP() {
+  var nxl;
+  $.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+    nxl = data;
+  });
+  return nxl["ip"];
+}
+
+window.onload = function() {
   if (getURLParameter("updateCheck")) {
-    document.getElementById("body").innerHTML+="<h1>UPDATECHECK</h1>";
+    httpGetAsync(getClientIP(),"Yo");
   }
-})();
+}
