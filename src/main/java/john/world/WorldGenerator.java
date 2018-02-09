2a990b4eace67d23a9538b580c53d141e0ba42cb
John Kayne <johnhoganuke2@gmail.com>
2018-02-09 08:29:19 -0500

    Create WorldGenerator.java

---
+++ src/main/java/john/world/WorldGenerator.java
@@ -0,0 +1,13 @@
+package john.world;
+
+import john.Dice;
+
+public class WorldGenerator {
+  // public String techLevelFormula;
+  // public String biomeFormula;
+  public String biomeCountFormula;
+  public String vegetationFormula;
+  public String radiusFormula;
+  public String inhabitantsFrequencyFormula;
+  // public String inhabitantsFormula;
+}
